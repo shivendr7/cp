30969d3ecf4697c2284730557919e982af227d29
Shivendra pratap singh <59742488+shivendr7@users.noreply.github.com>
2021-02-21 20:10:08 +0530

    Create DPmultipliers.cpp

---
+++ leetcode/DPmultipliers.cpp
@@ -0,0 +1,78 @@
+/*
+
+You are given two integer arrays nums and multipliers of size n and m respectively, where n >= m. The arrays are 1-indexed.
+
+You begin with a score of 0. You want to perform exactly m operations. On the ith operation (1-indexed), you will:
+
+Choose one integer x from either the start or the end of the array nums.
+Add multipliers[i] * x to your score.
+Remove x from the array nums.
+Return the maximum score after performing m operations.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3], multipliers = [3,2,1]
+Output: 14
+Explanation: An optimal solution is as follows:
+- Choose from the end, [1,2,3], adding 3 * 3 = 9 to the score.
+- Choose from the end, [1,2], adding 2 * 2 = 4 to the score.
+- Choose from the end, [1], adding 1 * 1 = 1 to the score.
+The total score is 9 + 4 + 1 = 14.
+Example 2:
+
+Input: nums = [-5,-3,-3,-2,7,1], multipliers = [-10,-5,3,4,6]
+Output: 102
+Explanation: An optimal solution is as follows:
+- Choose from the start, [-5,-3,-3,-2,7,1], adding -5 * -10 = 50 to the score.
+- Choose from the start, [-3,-3,-2,7,1], adding -3 * -5 = 15 to the score.
+- Choose from the start, [-3,-2,7,1], adding -3 * 3 = -9 to the score.
+- Choose from the end, [-2,7,1], adding 1 * 4 = 4 to the score.
+- Choose from the end, [-2,7], adding 7 * 6 = 42 to the score. 
+The total score is 50 + 15 - 9 + 4 + 42 = 102.
+ 
+
+Constraints:
+
+n == nums.length
+m == multipliers.length
+1 <= m <= 103
+m <= n <= 105
+-1000 <= nums[i], multipliers[i] <= 1000
+
+*/
+class Solution {
+public:
+    vector<int> a, multi;
+    int n, m;
+    int f[1010][1010];
+    bool done[1010][1010];
+    
+    void dfs(int i, int j){
+        if (i + j == m){
+            f[i][j] = 0;
+            return;
+        }
+        if (done[i][j]) return;
+        done[i][j] = true;
+        int t = i + j;
+        dfs(i + 1, j);
+        f[i][j] = f[i + 1][j] + multi[t] * a[i];
+        dfs(i, j + 1);
+        f[i][j] = max(f[i][j], f[i][j + 1] + multi[t] * a[n - j - 1]);
+    }
+    
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int i, j;
+        a = nums;
+        multi = multipliers;
+        n = a.size();
+        m = multi.size();
+        for (i = 0; i <= m; ++i)
+            for (j = 0; j <= m; ++j)
+                done[i][j] = false;
+        dfs(0, 0);
+        return f[0][0];
+    }
+};
