c05f2b115401a84d470ae9967ba91bb0c541d9a0
Shivendra pratap singh <59742488+shivendr7@users.noreply.github.com>
2021-04-17 22:46:24 +0530

    Create DPConsecutive1sNotAllowedO(1)Space.java

---
+++ geeks/DPConsecutive1sNotAllowedO(1)Space.java
@@ -0,0 +1,42 @@
+/*
+Given a positive integer N, count all possible distinct binary strings of length N such that there are no consecutive 1’s. Output your answer modulo 10^9 + 7.
+
+Example 1:
+
+Input:
+N = 3
+Output: 5
+Explanation: 5 strings are (000,
+001, 010, 100, 101).
+Example 2:
+
+Input:
+N = 2
+Output: 3
+Explanation: 3 strings are
+(00,01,10).
+Your Task:
+Complete the function countStrings() which takes single integer n, as input parameters and returns an integer denoting the answer. You don't to print answer or take inputs. 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ N ≤ 105
+*/
+//sol
+class Solution {
+    long countStrings(int n) {
+        // code here
+        long mod=(long)1e9+7;
+        long a=2,b=3;
+        if(n<3) return n==2?b:a;
+        long c=0;
+        for(int i=3;i<n+1;i++) {
+            c=(a+b)%mod;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
