91a36dbd68aa6d51652a0b729da32b0a1a009ec5
Shivendra pratap singh <59742488+shivendr7@users.noreply.github.com>
2021-05-05 08:49:45 +0530

    Create ArrayNonDecreasingApparentlySimple.py

---
+++ leetcode/ArrayNonDecreasingApparentlySimple.py
@@ -0,0 +1,37 @@
+"""
+link-https://leetcode.com/problems/non-decreasing-array/
+
+Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
+
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,3]
+Output: true
+Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+Example 2:
+
+Input: nums = [4,2,1]
+Output: false
+Explanation: You can't get a non-decreasing array by modify at most one element.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+-105 <= nums[i] <= 105
+"""
+#sol
+class Solution:
+    def checkPossibility(self, N: List[int]) -> bool:
+        err = 0
+        for i in range(1, len(N)):
+            if N[i] < N[i-1]:
+                if err or (i > 1 and i < len(N) - 1 and N[i-2] > N[i] and N[i+1] < N[i-1]):
+                    return False
+                err = 1
+        return True
