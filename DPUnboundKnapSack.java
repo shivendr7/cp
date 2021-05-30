b201498fb06143f7036ab64bbc129d84d53646b1
Shivendra pratap singh <59742488+shivendr7@users.noreply.github.com>
2021-05-30 22:11:58 +0530

    Update DPUnboundKnapSack.java

--- DPUnboundKnapSack.java
+++ DPUnboundKnapSack.java
@@ -54,3 +54,71 @@ class Solution
         return dp[n][amount]!=Integer.MAX_VALUE-1?dp[n][amount]:-1;
     }
 }
+
+/*
+Given an array cost[] of positive integers of size N and an integer W, cost[i] represents the cost of ‘i’ kg packet of oranges, the task is to find the minimum cost to buy W kgs of oranges. If it is not possible to buy exactly W kg oranges then the output will be -1
+
+Note:
+1. cost[i] = -1 means that ‘i’ kg packet of orange is unavailable
+2. It may be assumed that there is infinite supply of all available packet types.
+
+Example 1:
+
+Input: N = 5, arr[] = {20, 10, 4, 50, 100}
+W = 5
+Output: 14
+Explanation: choose two oranges to minimize 
+cost. First orange of 2Kg and cost 10. 
+Second orange of 3Kg and cost 4. 
+Example 2:
+
+Input: N = 5, arr[] = {-1, -1, 4, 3, -1}
+W = 5
+Output: -1
+Explanation: It is not possible to buy 5 
+kgs of oranges
+
+Your Task:  
+You don't need to read input or print anything. Complete the function minimumCost() which takes N, W, and array cost as input parameters and returns the minimum value.
+
+Expected Time Complexity: O(N*W)
+Expected Auxiliary Space: O(N*W)
+
+Constraints:
+1 ≤ N, W ≤ 103
+-1 ≤ cost[i] ≤ 105
+cost[i] ≠ 0
+*/
+class Solution
+{
+	public int minimumCost(int C[], int n,int W)
+	{
+		// Your code goes here
+		int cost[]=new int[n];
+		int wt[]=new int[n];
+		int N=0;
+		for(int i=0;i<n;i++) {
+		    if(C[i]!=-1) {
+		        wt[N]=i+1;
+		        cost[N++]=C[i];
+		    }
+		}
+		int dp[][]=new int[N+1][W+1];
+		
+		for(int i=1;i<=N;i++) dp[i][0]=0;
+		for(int j=0;j<=W;j++) dp[0][j]=100000000;
+		
+		for(int i=1;i<=N;i++) {
+		    for(int j=1;j<=W;j++) {
+		        if(j>=wt[i-1]) {
+		            dp[i][j]=Math.min(dp[i-1][j], cost[i-1]+dp[i][j-wt[i-1]]);
+		        }
+		        else {
+		            dp[i][j]=dp[i-1][j];
+		        }
+		    }
+		}
+		
+		return dp[N][W]==100000000?-1:dp[N][W];
+	}
+}
