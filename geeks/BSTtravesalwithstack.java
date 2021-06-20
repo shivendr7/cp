5cf73d0ff5db6ddde0d8c5252e911629cd0714c3
Shivendra pratap singh <59742488+shivendr7@users.noreply.github.com>
2021-06-20 20:05:53 +0530

    Update BSTtravesalwithstack.java

--- geeks/BSTtravesalwithstack.java
+++ geeks/BSTtravesalwithstack.java
@@ -1,5 +1,6 @@
 /*
-link- https://practice.geeksforgeeks.org/problems/find-a-pair-with-given-target-in-bst/1/?category[]=Binary%20Search%20Tree&category[]=Binary%20Search%20Tree&difficulty[]=1&page=1&query=category[]Binary%20Search%20Treedifficulty[]1page1category[]Binary%20Search%20Tree#
+link- https://practice.geeksforgeeks.org/problems/find-a-pair-with-given-target-in-bst/1/
+
 Given a Binary Search Tree and a target sum. Check whether there's a pair of Nodes in the BST with value summing up to the target sum. 
 
 Example 1:
@@ -102,5 +103,83 @@ class Solution
             st.push(p);
             p=p.right;
         } 
+    }
+}
+//cleaner code
+/*
+link-https://practice.geeksforgeeks.org/problems/merge-two-bst-s/1
+
+Given two BSTs, return elements of both BSTs in sorted form.
+
+Example 1:
+
+Input:
+BST1:
+       5
+     /   \
+    3     6
+   / \
+  2   4  
+BST2:
+        2
+      /   \
+     1     3
+            \
+             7
+            /
+           6
+Output: 1 2 2 3 3 4 5 6 6 7
+*/
+class Solution
+{
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1,Node root2)
+    {
+        // Write your code here
+        Stack<Node> st1=new Stack<>();
+        Node c1=root1;
+        Stack<Node> st2=new Stack<>();
+        Node c2=root2;
+        ArrayList<Integer> ans=new ArrayList<>();
+        
+        c1=trav(st1, c1);
+        c2=trav(st2, c2);
+        
+        while(c1!=null||c2!=null) {
+            if(c1==null) {
+                ans.add(c2.data);
+                c2=c2.right;
+                c2=trav(st2, c2);
+            }
+            else if(c2==null) {
+                ans.add(c1.data);
+                c1=c1.right;
+                c1=trav(st1, c1);
+            }
+            else {
+                if(c1.data<c2.data) {
+                    ans.add(c1.data);
+                    c1=c1.right;
+                    c1=trav(st1, c1);
+                }
+                else {
+                    ans.add(c2.data);
+                    c2=c2.right;
+                    c2=trav(st2, c2);
+                }
+            }
+        }
+        
+        return ans;
+    }
+    public Node trav(Stack<Node> s, Node curr) {
+        if(curr==null&&s.size()==0) return null;
+        while(curr!=null) {
+            s.push(curr);
+            curr=curr.left;
+        }
+        curr=s.pop();
+        return curr;
     }
 }
