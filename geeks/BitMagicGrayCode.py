d75214e675fb71bbb2697af7829d447dba14e325
Shivendra pratap singh <59742488+shivendr7@users.noreply.github.com>
2021-07-02 08:10:01 +0530

    Update BitMagicGrayCode.py

--- geeks/BitMagicGrayCode.py
+++ geeks/BitMagicGrayCode.py
@@ -1,4 +1,5 @@
 """
+https://leetcode.com/problems/gray-code/
 https://practice.geeksforgeeks.org/problems/gray-code-1611215248/1/
 https://www.geeksforgeeks.org/generate-n-bit-gray-codes/
 
@@ -41,3 +42,21 @@ class Solution:
         for x in range(2**n):
             print(x, x>>1, int(x^x>>1), bin(int(x^x>>1)))
         return [format(int(x) ^ x>>1, '0'+str(n)+'b') for x in range(2 ** n)]
+
+       
+"""
+The idea is that given a valid Gray Code of length 2^(n-1), we can construct a Gray Code of length 2^n by appending a 0 to the front of each binary representation (i.e., leave it unchanged), then reverse the order of our new list (to ensure the first and last element differ by a bit) and flip the 0's we just added to 1's (i.e., add 2^(n-1) to the base 10 representation)
+
+So in base 10. This would look like:
+
+n = 1, [0, 1]
+n = 2, [0, 1, 1+2, 0+2] = [0, 1, 3, 2]
+n = 3 [0, 1, 3, 2, 2+4, 3+4, 1+4, 0+4] = [0, 1, 3, 2, 6, 7, 5, 4]
+
+    
+"""
+def grayCode(self, n: int) -> List[int]:
+        baseCase = [0,1]
+        for k in range(1,n):
+            baseCase += [2**k + x for x in baseCase[::-1]]
+        return baseCase
