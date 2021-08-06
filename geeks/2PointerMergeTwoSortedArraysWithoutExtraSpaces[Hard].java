de1d6d6fcfeeddc4eef231306df4817df57df036
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-08-06 10:12:56 +0530

    Update 2PointerMergeTwoSortedArraysWithoutExtraSpaces[Hard].java

--- geeks/2PointerMergeTwoSortedArraysWithoutExtraSpaces[Hard].java
+++ geeks/2PointerMergeTwoSortedArraysWithoutExtraSpaces[Hard].java
@@ -1,5 +1,6 @@
 /*
-link-https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays5135/1#
+link-https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1#
+https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays5135/1#
 
 Given two sorted arrays arr1[] of size N and arr2[] of size M. Each array is sorted in non-decreasing order. Merge the two arrays into one sorted array in non-decreasing order without using any extra space.
 
