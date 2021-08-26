ba1bff7c578f7abc97eaacd4e6c4f790ece64a89
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-08-27 00:12:46 +0530

    Create BinarySearchSumOfSquareOfNumbers.java

---
+++ leetcode/BinarySearchSumOfSquareOfNumbers.java
@@ -0,0 +1,56 @@
+/*
+https://leetcode.com/problems/sum-of-square-numbers/
+
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+ 
+
+Example 1:
+
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+Example 2:
+
+Input: c = 3
+Output: false
+Example 3:
+
+Input: c = 4
+Output: true
+Example 4:
+
+Input: c = 2
+Output: true
+Example 5:
+
+Input: c = 1
+Output: true
+ 
+
+Constraints:
+
+0 <= c <= 231 - 1
+
+*/
+//sol
+public class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            int b = c - (int)(a * a);
+            if (binary_search(0, b, b))
+                return true;
+        }
+        return false;
+    }
+    public boolean binary_search(long s, long e, int n) {
+        if (s > e)
+            return false;
+        long mid = s + (e - s) / 2;
+        if (mid * mid == n)
+            return true;
+        if (mid * mid > n)
+            return binary_search(s, mid - 1, n);
+        return binary_search(mid + 1, e, n);
+    }
+}
