184cd50478c82fbc9689cabc47c65e4dc28fa713
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-09-11 16:51:43 +0530

    Update BFSJumpingNumbers.java

--- geeks/BFSJumpingNumbers.java
+++ geeks/BFSJumpingNumbers.java
@@ -2,7 +2,9 @@
 https://practice.geeksforgeeks.org/problems/jumping-numbers3805/1/
 
 Given a positive number X. Find the largest Jumping Number smaller than or equal to X. 
-Jumping Number: A number is called Jumping Number if all adjacent digits in it differ by only 1. All single digit numbers are considered as Jumping Numbers. For example 7, 8987 and 4343456 are Jumping numbers but 796 and 89098 are not.
+Jumping Number: A number is called Jumping Number if all adjacent digits in it differ by only 1.
+All single digit numbers are considered as Jumping Numbers. 
+For example 7, 8987 and 4343456 are Jumping numbers but 796 and 89098 are not.
 
  
 
