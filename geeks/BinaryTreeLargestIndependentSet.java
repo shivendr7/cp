72c4d5420bda5a4814aeeda129e2e39a7cb5e44c
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-09-15 23:59:59 +0530

    Create BinaryTreeLargestIndependentSet.java

---
+++ geeks/BinaryTreeLargestIndependentSet.java
@@ -0,0 +1,34 @@
+/*
+https://practice.geeksforgeeks.org/problems/largest-independent-set-problem/1/
+
+Given a Binary Tree of size N, find the size of the Largest Independent Set(LIS) in it. A subset of all tree nodes is an independent set if there is no edge between any two nodes of the subset. Your task is to complete the function LISS(), which finds the size of the Largest Independent Set.
+
+For example:
+Consider the following binary tree.The LIS is
+LIS: [10, 40, 60, 70, 80]
+Size: 5.
+
+Example1:
+
+Input:
+10 20 30 40 50 N 60 N N 70 80
+
+Output:
+5
+Explanation: LIS of the above 
+tree will be [10,40, 60, 70,80]
+Example 2:
+
+Input: 
+1 2 5 3 4 N 6
+
+Output:
+4
+ 
+
+Constraints:
+1<= Number of nodes <=500
+Your Task:
+You don't need to take input. Just complete the function LISS() that takes the root node as a parameter and returns the largest independent set.
+*/
+//sol
