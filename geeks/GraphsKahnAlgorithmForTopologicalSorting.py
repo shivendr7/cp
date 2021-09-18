1a3f6e0f86ff108ab7841768fc3f0d51e326848c
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-09-18 12:31:48 +0530

    Update GraphsKahnAlgorithmForTopologicalSorting.py

--- geeks/GraphsKahnAlgorithmForTopologicalSorting.py
+++ geeks/GraphsKahnAlgorithmForTopologicalSorting.py
@@ -3,9 +3,11 @@ https://practice.geeksforgeeks.org/problems/course-schedule/1/
 https://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/
 
 
-There are a total of n tasks you have to pick, labeled from 0 to n-1. Some tasks may have prerequisites tasks, for example to pick task 0 you have to first finish tasks 1, which is expressed as a pair: [0, 1]
+There are a total of n tasks you have to pick, labeled from 0 to n-1. Some tasks may have prerequisites tasks, for example to pick task 0 you 
+have to first finish tasks 1, which is expressed as a pair: [0, 1]
 Given the total number of n tasks and a list of prerequisite pairs of size m. Find a ordering of tasks you should pick to finish all tasks.
-Note: There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all tasks, return an empty array. Returning any correct order will give the output as 1, whereas any invalid order will give the output 0.
+Note: There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all tasks, return an empty array.
+Returning any correct order will give the output as 1, whereas any invalid order will give the output 0.
 
 
 Example 1:
