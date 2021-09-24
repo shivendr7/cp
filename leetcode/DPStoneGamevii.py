762f4336e41833c7c98bc1ddd7682c66c25eeb1a
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-09-25 01:44:49 +0530

    Update DPStoneGamevii.py

--- leetcode/DPStoneGamevii.py
+++ leetcode/DPStoneGamevii.py
@@ -165,3 +165,68 @@ def stoneGame(self, p):
             for i in range(n - d):
                 dp[i] = max(p[i] - dp[i + 1], p[i + d] - dp[i])
         return dp[0] > 0
+
+/*
+https://practice.geeksforgeeks.org/problems/optimal-strategy-for-a-game-1587115620/1/
+
+You are given an array A of size N. The array contains integers and is of even length. The elements of the array represent N coin of values V1, V2, ....Vn. You play against an opponent in an alternating way.
+
+In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of the coin.
+
+You need to determine the maximum possible amount of money you can win if you go first.
+Note: Both the players are playing optimally.
+
+Example 1:
+
+Input:
+N = 4
+A[] = {5,3,7,10}
+Output: 15
+Explanation: The user collects maximum
+value as 15(10 + 5)
+Example 2:
+
+Input:
+N = 4
+A[] = {8,15,3,7}
+Output: 22
+Explanation: The user collects maximum
+value as 22(7 + 15)
+Your Task:
+Complete the function maximumAmount() which takes an array arr[] (represent values of N coins) and N as number of coins as a parameter and returns the maximum possible amount of money you can win if you go first.
+
+Expected Time Complexity : O(N*N)
+Expected Auxiliary Space: O(N*N)
+
+Constraints:
+2 <= N <= 103
+1 <= Ai <= 106
+*/
+//sol
+class solve
+{
+    //Function to find the maximum possible amount of money we can win.
+    static long countMaximum(int arr[], int n)
+    {
+        // Your code here
+        long me=0, opp=0;
+        long dp[][]=new long[n][n];
+        
+        for(int i=0;i<n;i++) dp[i][i]=arr[i];
+        
+        for(int d=1;d<n;d++) {          //gap
+            for(int i=0;i<n-d;i++) {    //iter
+                int j=i+d;
+                if(d==1) {
+                    dp[i][j]=Math.max(arr[i], arr[j]);
+                }
+                else {
+                    long v1=arr[i]+Math.min(dp[i+2][j], dp[i+1][j-1]);
+                    long v2=arr[j]+Math.min(dp[i][j-2], dp[i+1][j-1]);
+                    dp[i][j]=Math.max(v1, v2);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+}
