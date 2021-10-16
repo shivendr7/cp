61ad3caa8e183a41fee32005b59e0eabef2e9577
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-10-16 11:57:45 +0530

    Update DPCoinChangeVar-2.java

--- geeks/DPCoinChangeVar-2.java
+++ geeks/DPCoinChangeVar-2.java
@@ -1,5 +1,6 @@
 /*
-Given a value V and array coins[] of size M, the task is to make the change for V cents, given that you have an infinite supply of each of coins{coins1, coins2, ..., coinsm} valued coins. Find the minimum number of coins to make the change. If not possible to make change then output -1
+Given a value V and array coins[] of size M, the task is to make the change for V cents, given that you have an infinite supply of each of coins{coins1, coins2, ..., coinsm}
+valued coins. Find the minimum number of coins to make the change. If not possible to make change then output -1
 
 Example 1:
 
