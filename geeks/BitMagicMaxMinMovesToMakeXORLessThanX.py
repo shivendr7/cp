26783b76db9fb378a71380d7d28889dfacfef406
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-10-26 23:57:09 +0530

    Update and rename BitMagicMaxXORLessThanX.py to BitMagicMaxMinMovesToMakeXORLessThanX.py

--- geeks/BitMagicMaxMinMovesToMakeXORLessThanX.py
+++ geeks/BitMagicMaxMinMovesToMakeXORLessThanX.py
@@ -49,3 +49,28 @@ class Solution:
                     ct+=1 
             ans=min(ans, N-ct) 
         return ans 
+    
+    """
+    java sol
+    class Solution 
+{ 
+    int count(int N, int A[], int X) 
+    {   
+        // code here 
+        int prefix=0, ans=N;
+        for(int i=30;i>=0;i--){
+            if(((X>>i)&1)!=0){
+                prefix^=(1l<<i);
+                continue;
+            }
+            int ct=0;
+            long p=prefix^(1l<<i);
+            for(int j=0;j<N;j++){
+                if((A[j]&p)==p)
+                    ct++;
+            }
+            ans=Math.min(ans, N-ct);
+        }
+        return ans;
+    }
+    """
