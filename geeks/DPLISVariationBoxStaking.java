b1b8a98e76db8bfe926d7b2b7b0da7db791245eb
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-11-15 23:52:35 +0530

    Update DPLISVariationBoxStaking.java

--- geeks/DPLISVariationBoxStaking.java
+++ geeks/DPLISVariationBoxStaking.java
@@ -1,5 +1,17 @@
 /*
-You are given a set of N types of rectangular 3-D boxes, where the ith box has height h, width w and length l. You task is to create a stack of boxes which is as tall as possible, but you can only stack a box on top of another box if the dimensions of the 2-D base of the lower box are each strictly larger than those of the 2-D base of the higher box. Of course, you can rotate a box so that any side functions as its base.It is also allowable to use multiple instances of the same type of box. You task is to complete the function maxHeight which returns the height of the highest possible stack so formed.
+
+LIS Construction: https://leetcode.com/problems/largest-divisible-subset/discuss/1578991/C%2B%2B-4-Simple-Solutions-w-Detailed-Explanation-or-Optimizations-from-Brute-Force-to-DP
+
+*/
+
+/*
+
+
+You are given a set of N types of rectangular 3-D boxes, where the ith box has height h, width w and length l.
+You task is to create a stack of boxes which is as tall as possible, but you can only stack a box on top of another 
+box if the dimensions of the 2-D base of the lower box are each strictly larger than those of the 2-D base of the 
+higher box. Of course, you can rotate a box so that any side functions as its base.It is also allowable to use multiple
+instances of the same type of box. You task is to complete the function maxHeight which returns the height of the highest possible stack so formed.
  
 
 Note: 
