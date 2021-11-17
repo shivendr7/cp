b18d0851a4116e9a07b05787c6bfe6ab3e6e5805
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-11-17 23:22:24 +0530

    Update StringPatternSearchZAlgo.java

--- geeks/StringPatternSearchZAlgo.java
+++ geeks/StringPatternSearchZAlgo.java
@@ -76,3 +76,64 @@ class Solution
         return res;
     }
 }
+
+/*
+easy identify pattern 
+https://practice.geeksforgeeks.org/problems/match-specific-pattern/1/
+
+Given a dictionary of words and a pattern. Every character in the pattern is uniquely mapped to a character in the dictionary. Find all such words in the dictionary that match the given pattern. 
+
+Example 1:
+
+Input:
+N = 4
+dict[] = {abb,abc,xyz,xyy}
+pattern  = foo
+Output: abb xyy
+Explanation: xyy and abb have the same
+character at index 1 and 2 like the
+pattern.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findMatchedWords() which takes an array of strings dict[] consisting of the words in the dictionary and a string, Pattern and returns an array of strings consisting of all the words in the dict[] that match the given Pattern in lexicographical order.
+
+Expected Time Complexity: O(N*K) (where K is the length of the pattern).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= N <= 10
+
+*/
+//sol
+class GfG
+{
+/* The function returns an array of strings 
+present in the dictionary which matches
+the string pattern.
+You are required to complete this method */
+public static ArrayList<String> findMatchedWords(ArrayList<String> dict, String pattern)
+	{
+	    //add code here. 
+	     ArrayList<String> res=new ArrayList<String>();
+          for(String word:dict){
+              if(word.length()!=pattern.length()){
+                  continue;
+              }
+              int hash1[]=new int[256];
+              int hash2[]= new int[256];
+              int i;
+              for( i=0;i<word.length();i++){
+                if(hash1[word.charAt(i)]==hash2[pattern.charAt(i)])  {
+                    hash1[word.charAt(i)]++;
+                    hash2[pattern.charAt(i)]++;
+                }
+                else{
+                    break;
+                }
+              }
+              if(i==word.length()){
+                  res.add(word);
+              }
+          }
+         return res;
+	}
+}
