fb50d5a12170af78c41aee763b71989d3d083c01
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-12-06 23:52:51 +0530

    Update BinarySearchDivideConquerKthElementInTwoSortedArrays.java

--- geeks/BinarySearchDivideConquerKthElementInTwoSortedArrays.java
+++ geeks/BinarySearchDivideConquerKthElementInTwoSortedArrays.java
@@ -1,7 +1,8 @@
 /*
 https://practice.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1#
 
-Given two sorted arrays arr1 and arr2 of size N and M respectively and an element K. The task is to find the element that would be at the k’th position of the final sorted array.
+Given two sorted arrays arr1 and arr2 of size N and M respectively and an element K. 
+The task is to find the element that would be at the k’th position of the final sorted array.
  
 
 Example 1:
