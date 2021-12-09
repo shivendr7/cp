adae8880cac69df55df6cbd21736c9b093082e32
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-12-09 22:28:26 +0530

    Update ArraysBoyerMooreVotingAlgoO1Space.java

--- geeks/ArraysBoyerMooreVotingAlgoO1Space.java
+++ geeks/ArraysBoyerMooreVotingAlgoO1Space.java
@@ -1,7 +1,8 @@
 /*
 https://practice.geeksforgeeks.org/problems/majority-element-1587115620/1/
 
-Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 
+times in the array.
  
 
 Example 1:
