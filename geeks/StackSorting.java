8beb44f348e9bb35a6f7dd5185ab3376e4aa0993
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-12-19 00:25:19 +0530

    Update StackSorting.java

--- geeks/StackSorting.java
+++ geeks/StackSorting.java
@@ -50,3 +50,72 @@ class GfG{
 		return s;
 	}
 }
+
+/*
+https://practice.geeksforgeeks.org/problems/max-length-removal0452/1/
+
+Given a binary string (consists of only 0 and 1). If there is “100” as a sub-string in the string, then we can delete this sub-string. The task is to find the length of longest sub-string which can be make removed?
+
+Example 1:
+
+Input  : 
+str = "1011100000100"
+Output :
+6
+Explanation :
+Sub-strings present in str that can be make
+removed 101{110000}0{100}. First
+sub-string 110000-->100-->null, length is = 6.
+Second sub-string 100-->null, length is = 3
+ 
+
+Example 2:
+
+Input  :
+str = "111011"
+Output :
+0
+Explanation :
+There is no sub-string which can be make null.
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function longestNull() which takes the string S[] as inputs and returns the length of the longest string that can be removed.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ string length ≤ 104
+S[i] = {0, 1}
+*/
+//sol
+
+class Solution{
+    public:
+        
+    int longestNull(string str)
+    {
+        // Your code goes here   
+        vector<pair<char,int>> arr;
+        arr.push_back({'$',-1});
+        int max_substr=0;
+        for(int i=0;i<str.length();i++) {
+            arr.push_back({str[i],i}); 
+            while(arr.size()>=3 &&
+                    arr[arr.size()-3].first=='1' &&
+                    arr[arr.size()-2].first=='0' &&
+                    arr[arr.size()-1].first=='0')
+            {
+                arr.pop_back();
+                arr.pop_back();
+                arr.pop_back();
+            }
+            int temp=arr.back().second;
+            max_substr=max(max_substr,i-temp);
+        }
+        return max_substr;
+    }
+};
