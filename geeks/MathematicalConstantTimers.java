f2cadcca0000fa0bf8ba4877ea32c533e9864f01
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-12-20 22:33:44 +0530

    Update MathematicalConstantTimers.java

--- geeks/MathematicalConstantTimers.java
+++ geeks/MathematicalConstantTimers.java
@@ -47,3 +47,46 @@ static long find(long A,long B){
         i=B-(A%B);
         return A+i;
     } 
+
+
+/*
+https://practice.geeksforgeeks.org/problems/nth-number-made-of-prime-digits4319/1/
+
+Given a number 'N'. The task is to find the Nth number whose each digit is a prime number i.e 2, 3, 5, 7. In other words you have to find nth number of this sequence : 2, 3, 5, 7, 22, 23 ,.. and so on.
+
+Example 1:
+
+Input:
+N = 10
+Output: 33
+Explanation:10th number in the sequence of
+numbers whose each digit is prime is 33.
+Example 2:
+
+Input:
+N = 21
+Output: 222
+Explanation:21st number in the sequence of
+numbers whose each digit is prime is 222.
+Your Task:
+Complete primeDigits function and return the nth number of the given sequence made of prime digits.
+
+Constraints:
+1 <= N <= 100 
+*/
+//sol
+
+  public static int primeDigits(int n)
+    {
+        //Your code here
+         int arr[]={7,2,3,5};
+         int res=0,weight=1;
+         while(n>0){
+             int rem = n%4;
+             if(rem==0) n = (n-1)/4;
+             else n=n/4;
+             res += arr[rem]*weight;
+             weight *= 10;
+         }
+         return res;
+    }
