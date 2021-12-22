181d6e7e92b9f1fb3f7927f67892ae3f3a91da47
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-12-22 22:53:59 +0530

    Update MathematicalConstantTimers.java

--- geeks/MathematicalConstantTimers.java
+++ geeks/MathematicalConstantTimers.java
@@ -90,3 +90,54 @@ Constraints:
          }
          return res;
     }
+
+
+/*
+https://practice.geeksforgeeks.org/problems/sum-of-subset-differences/1/
+
+Given an array S consisting of n numbers, find the sum of difference between last and first element of each subset.
+For example for set 5,8 there are 3 subsets possible
+5    (difference is 5-5 = 0 as 5 is the only last and first element)
+8    (difference is 8-8 = 0 as 8 is the only last and first element)
+5 8 (difference is 8-5 = 3 as 8 is the last element  and 5 is the first element)
+thus difference sum is 0+0+3 = 3
+You are required to complete the function which returns an integer denoting the sum of difference between last and first element for each subset of array S.
+
+Note: Elements in the subset should be in the same order as in the set S.
+
+Input:
+The first line of input contains an integer T denoting the no of test cases. Then T test cases follow. Each test case contains
+2 lines the first line of input contains an integer N denoting the size of the array A. In the next line are N space separated values of the array A.
+
+Output:
+For each test case in a new line output will be the sum of difference between last and first element of each subset of array S.
+
+Constraints:
+1<=T<=100
+1<=N<=20
+1<=A[]<=1000
+
+Example(To be used only for expected output) :
+Input:
+2
+4
+5 2 9 6
+3
+1 2 3 
+Output
+21
+6
+
+Note:The Input/Ouput format and Example given are used for system's internal purpose, and should be used by a user for Expected Output only. 
+As it is a function problem, hence a user should not read any input from stdin/console. The task is to complete the function specified, and not to write the full code.
+
+
+
+//sol
+def sumDiff(S, n):
+    #Code here
+    s=0
+    for i in range(len(S)):
+        s+=(2**i-2**(n-i-1))*S[i]
+    return s
+*/
