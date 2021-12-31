094fe7976d814f958885e6509250d828df07e930
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2021-12-31 23:57:00 +0530

    Update ArraysAlternateRearrange.java

--- geeks/ArraysAlternateRearrange.java
+++ geeks/ArraysAlternateRearrange.java
@@ -65,9 +65,82 @@ class Solution{
 // array elements back to it's original form
         for (int i = 0; i < n; i++)
             arr[i] = arr[i] / max_elem;
-
-
-        
     }
     
+}
+
+
+/*
+https://practice.geeksforgeeks.org/problems/rearrange-an-array-such-that-arri-i3618/1/
+https://practice.geeksforgeeks.org/problems/reorganize-the-array4810/1/
+
+Given an array of size N that has elements ranging from 0 to N-1. All elements may not be present in the array. If the element is not present then there will be -1 present in the array. Rearrange the array such that A[i] = i, and if i is not present, display -1 at that place.
+
+Example 1:
+
+â€‹Input : arr[ ] = {-1, -1, 6, 1, 9, 3, 2, 
+                              -1, 4, -1}
+Output : -1 1 2 3 4 -1 6 -1 -1 9
+Explanation:
+In range 0 to 9, all except 0, 5, 7 and 8 
+are present. Hence, we print -1 instead of 
+them.
+
+Example 2:
+
+Input : arr[ ] = {0, 1, 2, 3, 4, 5} 
+Output : 0 1 2 3 4 5
+Your Task:
+
+This is a function problem. The input is already taken care of by the driver code. You only need to complete the function modifyArray() that takes an array (arr), sizeOfArray (n), and return the modified array. The driver code takes care of the printing.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+ 
+
+Constraints:
+1 ≤ N ≤ 105
+0 ≤ A[i] ≤ N-1
+
+*/
+//sol
+
+class Solution{
+    // Function for finding maximum and value pair
+    public static int[] modifyArray (int arr[], int n) {
+        // Complete the function 
+        int x=0, temp=0;
+        for(int i=0;i<n;i++) {
+            if(arr[i]!=-1 && arr[i]!=i) {
+                x=arr[i];
+                while(arr[x]!=x && arr[x]!=-1) {
+                    temp=arr[x];
+                    arr[x]=x;
+                    x=temp;
+                }
+                arr[x]=x;
+                if(arr[i]!=i) 
+                    arr[i]=-1;
+            }
+        }
+        return arr;
+    }
+}
+//O(n)
+int * Rearrange(int *arr,int n){
+    // Complete the function
+    int i=0;
+   while(i<n)
+   {
+       if(arr[i]!=i && arr[i]!=-1)
+       {
+           swap(arr[i], arr[arr[i]]);
+       }
+       else
+       {
+           i++;
+       }
+   }
+   return arr;
 }
