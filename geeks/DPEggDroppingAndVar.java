af25ce94d069c2fba34d91704a365b280900d559
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-01-01 23:39:39 +0530

    Update and rename DPEggDropping.java to DPEggDroppingAndVar.java

--- geeks/DPEggDroppingAndVar.java
+++ geeks/DPEggDroppingAndVar.java
@@ -59,3 +59,67 @@ class Solution
 }
 
 // O(NlogK) APproach=> https://leetcode.com/problems/super-egg-drop/solution/
+
+/*
+https://leetcode.com/problems/burst-balloons/
+
+You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+
+If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+
+Return the maximum coins you can collect by bursting the balloons wisely.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+Example 2:
+
+Input: nums = [1,5]
+Output: 10
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 500
+0 <= nums[i] <= 100
+
+*/
+//sol
+/*
+py sol
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        a=[1]+nums+[1]
+        n=len(nums)+2
+        dp=[[0]*n for _ in range(n)]
+	
+        """
+        not working :(
+        
+        for i in range(1, n):
+            for j in range(i):
+                MAX=0
+                for k in range(j+1, i):
+                    MAX=max(MAX, dp[j][k] + a[j]*a[k]*a[i] + dp[k][i])
+                dp[j][i]=MAX
+                
+        return dp[0][n-1]
+        """
+	
+        for i in range(n-2, -1, -1):
+            for j in range(i+2, n):
+                MAX=0
+                for k in range(i+1, j):
+                    MAX=max(MAX, dp[i][k] + a[i]*a[k]*a[j] + dp[k][j])
+                dp[i][j]=MAX
+        return dp[0][n-1]
+    
+*/
