f696e1e237949e57030dca86a46bcd158eb36926
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-01-16 00:27:17 +0530

    Create BFSRecursiveJumpGame4.py

---
+++ leetcode/BFSRecursiveJumpGame4.py
@@ -0,0 +1,69 @@
+"""
+https://leetcode.com/problems/jump-game-iv/
+
+Given an array of integers arr, you are initially positioned at the first index of the array.
+
+In one step you can jump from index i to index:
+
+i + 1 where: i + 1 < arr.length.
+i - 1 where: i - 1 >= 0.
+j where: arr[i] == arr[j] and i != j.
+Return the minimum number of steps to reach the last index of the array.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [100,-23,-23,404,100,23,23,23,3,404]
+Output: 3
+Explanation: You need three jumps from index 0 --> 4 --> 3 --> 9. Note that index 9 is the last index of the array.
+Example 2:
+
+Input: arr = [7]
+Output: 0
+Explanation: Start index is the last index. You do not need to jump.
+Example 3:
+
+Input: arr = [7,6,9,6,9,6,9,7]
+Output: 1
+Explanation: You can jump directly from index 0 to index 7 which is last index of the array.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5 * 104
+-108 <= arr[i] <= 108
+
+"""
+#sol
+class Solution:
+    
+    def minJumps(self, arr: List[int]) -> int:
+        N, groups = len(arr), defaultdict(list)
+
+        for i, el in enumerate(arr): 
+            groups[el].append(i)
+
+        vis, vis_groups = set(), set()
+        
+        def bfs(lvl, dist):
+            nextLvl = set()
+            
+            for i in lvl:
+                if i in vis: continue
+                if i == N-1: return dist
+                
+                vis.add(i)
+                
+                if i: nextLvl.add(i-1)
+                if i+1 < N: nextLvl.add(i+1)
+                
+                if not arr[i] in vis_groups:
+                    vis_groups.add(arr[i])
+                    nextLvl.update(groups[arr[i]])
+            
+            return bfs(nextLvl, dist + 1)
+            
+        return bfs(set([0]), 0)
