514a6b272fa0646a1e2b3fc487681f85bb09f3e3
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-01-16 10:13:15 +0530

    Update MathematicalMisc.py

--- leetcode/MathematicalMisc.py
+++ leetcode/MathematicalMisc.py
@@ -52,3 +52,95 @@ class Solution:
             if not r: return length
             length += 1
             r = 10*r + 1
+
+           
+           
+"""
+https://leetcode.com/problems/minimum-moves-to-reach-target-score/
+
+target range till 10**9
+
+You are playing a game with integers. You start with the integer 1 and you want to reach the integer target.
+
+In one move, you can either:
+
+Increment the current integer by one (i.e., x = x + 1).
+Double the current integer (i.e., x = 2 * x).
+You can use the increment operation any number of times, however, you can only use the double operation at most maxDoubles times.
+
+Given the two integers target and maxDoubles, return the minimum number of moves needed to reach target starting with 1.
+
+ 
+
+Example 1:
+
+Input: target = 5, maxDoubles = 0
+Output: 4
+Explanation: Keep incrementing by 1 until you reach target.
+Example 2:
+
+Input: target = 19, maxDoubles = 2
+Output: 7
+Explanation: Initially, x = 1
+Increment 3 times so x = 4
+Double once so x = 8
+Increment once so x = 9
+Double again so x = 18
+Increment once so x = 19
+Example 3:
+
+Input: target = 10, maxDoubles = 4
+Output: 4
+Explanation: Initially, x = 1
+Increment once so x = 2
+Double once so x = 4
+Increment once so x = 5
+Double again so x = 10
+ 
+
+Constraints:
+
+1 <= target <= 109
+0 <= maxDoubles <= 100
+
+"""
+"""
+mine; BFS; TLE
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        q = [[1, maxDoubles]]
+        step = 0
+        ans = 10**9
+        while True:
+            l = len(q)
+            c = 0
+            for _ in range(l):
+                out = q.pop(0)
+                if out[0] == target:
+                    return min(ans, step)
+                q.append([out[0]+1, out[1]])
+                if out[1] > 0:
+                    q.append([out[0]*2, out[1]-1])
+                else:
+                    c += 1
+                    ans = min(ans, step+target-out[0])
+            step += 1
+            if c == l:
+                return ans
+        return -1
+"""
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        to_ret = 0
+        while target > 1 :
+            if maxDoubles == 0 :
+                to_ret += target - 1
+                break
+            if target % 2 == 1 :
+                to_ret += 1
+                target -= 1
+            else :
+                to_ret += 1
+                target = target // 2
+                maxDoubles -= 1
+        return to_ret
