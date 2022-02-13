91e4d6425d6889dc56d72594e09c7d48be42d9a7
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-02-13 22:04:58 +0530

    Update ArraysMathematicalCountOfSubarraysWithMaxLessThanK.java

--- geeks/ArraysMathematicalCountOfSubarraysWithMaxLessThanK.java
+++ geeks/ArraysMathematicalCountOfSubarraysWithMaxLessThanK.java
@@ -23,7 +23,8 @@ Output: 9
 Explanation: There are 9 subarrays having
 value strictly greater than K.
 Your Task:
-Complete the function countSubarray() which takes an array arr, two integers n, k, as input parameters and returns an integer denoting the answer. You don't to print answer or take inputs.
+Complete the function countSubarray() which takes an array arr, two integers n, k, as input parameters and returns an integer denoting the answer. You don't to print
+answer or take inputs.
 
 Expected Time Complexity: O(N)
 Expected Auxiliary Space: O(1)
