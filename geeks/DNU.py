59e5a61a7b9f1ab3fbd10ed367168da12d754b88
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-02-16 23:31:29 +0530

    Update DNU.py

--- geeks/DNU.py
+++ geeks/DNU.py
@@ -1,6 +1,8 @@
 """
-A gallery with plants is divided into n parts, numbered : 0,1,2,3...n-1. There are provisions for attaching water sprinklers at every partition. A sprinkler with range x at partition i can water all partitions from i-x to i+x.
-Given an array gallery[ ] consisting of n integers, where gallery[i] is the range of sprinkler at partition i (power==-1 indicates no sprinkler attached), return the minimum number of sprinklers that need to be turned on to water the complete gallery.
+A gallery with plants is divided into n parts, numbered : 0,1,2,3...n-1. There are provisions for attaching water sprinklers at every partition. 
+A sprinkler with range x at partition i can water all partitions from i-x to i+x.
+Given an array gallery[ ] consisting of n integers, where gallery[i] is the range of sprinkler at partition i (power==-1 indicates no sprinkler attached),
+return the minimum number of sprinklers that need to be turned on to water the complete gallery.
 If there is no possible way to water the full length using the given sprinklers, print -1.
 
 Example 1:
