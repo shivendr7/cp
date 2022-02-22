8709c41feddc7d4a246e30ab2699d2f4fa31a9bd
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-02-23 00:04:44 +0530

    Update BitMagicNumberDivisibleBy3.java

--- geeks/BitMagicNumberDivisibleBy3.java
+++ geeks/BitMagicNumberDivisibleBy3.java
@@ -1,7 +1,8 @@
 /*
 https://practice.geeksforgeeks.org/problems/is-binary-number-multiple-of-30654/1/
 
-Given a binary number, Find, if given binary number is a multiple of 3. The given number can be big upto 2^10000. It is recommended to finish the task using one traversal of input binary string.
+Given a binary number, Find, if given binary number is a multiple of 3. The given number can be big upto 2^10000. It is recommended to finish the task using one
+traversal of input binary string.
 
 Example 1:
 
@@ -16,7 +17,8 @@ Output: 0
 Explanation: "100" decimal equivalent
 is 4, which is not divisible by 3.
 Your Task:  
-You don't need to read input or print anything. Your task is to complete the function isDivisible() which takes the string s as inputs and returns 1 if the given number is divisible by 3 otherwise 0.
+You don't need to read input or print anything. Your task is to complete the function isDivisible() which takes the string s as inputs and returns 1 if the given 
+number is divisible by 3 otherwise 0.
 
 Expected Time Complexity: O(|S|)
 Expected Auxiliary Space: O(1)
