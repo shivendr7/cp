2f3d9cc4ffeb8578af9ffb27d1ff50b167d31cd1
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-03-06 23:12:41 +0530

    Update MathematicalConstantTimers.java

--- geeks/MathematicalConstantTimers.java
+++ geeks/MathematicalConstantTimers.java
@@ -397,3 +397,51 @@ class Solution:
         temp=((n)*(n-1))//2
         temp+=3+(2*(n-1))
         return (temp)
+
+/*
+https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
+https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/discuss/1825668/Java-Easy-Solution:-Pure-Permutation-based
+
+Given n orders, each order consist in pickup and delivery services. 
+
+Count all valid pickup/delivery possible sequences such that delivery(i) is always after of pickup(i). 
+
+Since the answer may be too large, return it modulo 10^9 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: 1
+Explanation: Unique order (P1, D1), Delivery 1 always is after of Pickup 1.
+Example 2:
+
+Input: n = 2
+Output: 6
+Explanation: All possible orders: 
+(P1,P2,D1,D2), (P1,P2,D2,D1), (P1,D1,P2,D2), (P2,P1,D1,D2), (P2,P1,D2,D1) and (P2,D2,P1,D1).
+This is an invalid order (P1,D2,P2,D1) because Pickup 2 is after of Delivery 2.
+Example 3:
+
+Input: n = 3
+Output: 90
+ 
+
+Constraints:
+
+1 <= n <= 500
+*/
+class Solution:
+    def countOrders(self, n: int) -> int:
+        MOD = 1_000_000_007
+        ans = 1
+
+        for i in range(1, n + 1):
+            # Ways to arrange all pickups, 1*2*3*4*5*...*n
+            ans = ans * i
+            # Ways to arrange all deliveries, 1*3*5*...*(2n-1)
+            ans = ans * (2 * i - 1)
+            ans %= MOD
+        
+        return ans
