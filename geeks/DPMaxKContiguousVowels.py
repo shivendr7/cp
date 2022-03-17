66a193635fe36a56f5b0e1cdfe9b318540ad47cd
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-03-18 00:52:58 +0530

    Update DPMaxKContiguousVowels.py

--- geeks/DPMaxKContiguousVowels.py
+++ geeks/DPMaxKContiguousVowels.py
@@ -61,3 +61,20 @@ class Solution:
                 tot = (tot + count) % mod
             dp.append(tot)
         return dp[-1]
+
+
+"""
+int kvowelwords(int n, int k)
+     {
+      vector<vector<ll>>dp(n+1,vector<ll>(k+1,1));
+      for(int i=1;i<=n;i++)
+      {
+          for(int j=0;j<=k;j++)
+          {
+              if(j==0) dp[i][j]=21*dp[i-1][k]%mod;
+              else  dp[i][j]=(5*dp[i-1][j-1]%mod+21*dp[i-1][k]%mod)%mod;
+          }
+      }
+      return dp[n][k];
+  }
+"""
