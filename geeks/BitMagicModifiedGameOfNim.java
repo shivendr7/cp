ea90972dbc29fa3aa75d05db3dc2a13e030c9154
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-04-17 23:22:07 +0530

    Update BitMagicModifiedGameOfNim.java

--- geeks/BitMagicModifiedGameOfNim.java
+++ geeks/BitMagicModifiedGameOfNim.java
@@ -48,3 +48,82 @@ class Solution{
         return 2;
     }
 }
+
+
+/*
+https://practice.geeksforgeeks.org/problems/the-bit-game2313/1/
+must see editorial
+
+Two players, Player 1 and Player 2, are given an integer N to play a game. The rules of the game are as follows :
+1. In one turn, a player can swap any 2 bits of N in its binary representation to make a new N.
+2. In one turn, a player has to make a number strictly less than N.
+3. Player 1 always takes first turn.
+4. If a player cannot make a move, he loses.
+Assume that both the players play optimally.
+
+ 
+
+Example 1:
+
+Input:
+N = 8
+Output:
+1
+Explanation:
+N = 8
+N = 1000 (binary)
+Player 1 swaps the 1st 
+and 4th bit.
+1000
+N = 0001
+Player 2 cannot make a move, 
+so Player 1 wins.
+ 
+
+ 
+
+Example 2:
+
+Input:
+N = 1
+Output:
+2
+Explanation:
+N = 1
+Player 1 cannot make 
+a move, so Player 2 wins.
+ 
+
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function swapBitGame() which takes an integer N and returns "1" if Player 1 wins, else return "2".
+
+ 
+
+Expected Time Complexity: O(log(N))
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints : 
+1 <= N <= 10^12
+
+*/
+
+//sol
+class Solution:
+    def swapBitGame (ob,N):
+        # code here 
+        zeros=0
+        x0r=0
+        while (N) :
+           if (N&1) :
+             x0r ^= zeros
+           else :
+             zeros=zeros+1
+           N>>=1
+
+        if x0r > 0 :
+          return 1
+        else :
+          return 2
