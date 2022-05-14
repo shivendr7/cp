50d09bd02e43b2e7801686dde868100d263b50e9
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-05-14 22:29:38 +0530

    Create GreedyPrefixBinaryMaxTilesCovered.py

---
+++ leetcode/GreedyPrefixBinaryMaxTilesCovered.py
@@ -0,0 +1,73 @@
+"""
+https://leetcode.com/problems/maximum-white-tiles-covered-by-a-carpet/
+
+You are given a 2D integer array tiles where tiles[i] = [li, ri] represents that every tile j in the range li <= j <= ri is colored white.
+
+You are also given an integer carpetLen, the length of a single carpet that can be placed anywhere.
+
+Return the maximum number of white tiles that can be covered by the carpet.
+
+ 
+
+Example 1:
+
+
+Input: tiles = [[1,5],[10,11],[12,18],[20,25],[30,32]], carpetLen = 10
+Output: 9
+Explanation: Place the carpet starting on tile 10. 
+It covers 9 white tiles, so we return 9.
+Note that there may be other places where the carpet covers 9 white tiles.
+It can be shown that the carpet cannot cover more than 9 white tiles.
+Example 2:
+
+
+Input: tiles = [[10,11],[1,1]], carpetLen = 2
+Output: 2
+Explanation: Place the carpet starting on tile 10. 
+It covers 2 white tiles, so we return 2.
+ 
+
+Constraints:
+
+1 <= tiles.length <= 5 * 104
+tiles[i].length == 2
+1 <= li <= ri <= 109
+1 <= carpetLen <= 109
+The tiles are non-overlapping.
+
+"""
+#sol
+# Explanation:
+
+# Sort the tiles by the starting position.
+# Build the prefix sum array to store the length sum of tiles.
+# Travese each tile, given its starting position s, we know that the furthest position that the carpet can cover to is s+carpetLen-1. Having the furthest position, we binary search the index of the ending tile that the carpet can partially cover. Note that in this step, we greedily align the starting position of the carpet with the starting position of the tile.
+# Calculate the length of the ending tile that the carpet cannot partially cover. This is the compensate part that we need to subtract at last.
+
+class Solution:
+    def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
+        # sort the tiles by the starting position
+        tiles.sort(key = lambda x:x[0])
+        # build the starting position array
+        startPos = [tiles[i][0] for i in range(len(tiles))]
+        # build the prefix sum array
+        preSum = [0] * (len(tiles) + 1)
+        for i in range(1, len(tiles) + 1):
+            preSum[i] = preSum[i - 1] + (tiles[i-1][1]-tiles[i-1][0] + 1)
+        
+        res = 0
+        for i in range(len(tiles)):
+            s, e = tiles[i]
+            # if the length of tile >= length of carpet, return carpetLen
+            if e >= s + carpetLen - 1:
+                return carpetLen
+            # binary search the index of the ending tile that the carpet can partially cover
+            endIdx = bisect_right(startPos, s + carpetLen - 1) - 1
+            # calculate the length of the ending tile that the carpet cannot cover 
+            compensate = 0
+            if tiles[endIdx][1] > s + carpetLen - 1:
+                compensate = tiles[endIdx][1] - s - carpetLen + 1
+            # update the result
+            res = max(res, preSum[endIdx+1] - preSum[i] - compensate)
+            
+        return res
