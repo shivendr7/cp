b2b8e11b1e8c4b58c76ab0dbb2e04b71930de707
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2022-12-18 05:36:51 +0530

    Update BinarySearchInSortedAndRotatedArray.java

--- geeks/BinarySearchInSortedAndRotatedArray.java
+++ geeks/BinarySearchInSortedAndRotatedArray.java
@@ -1,41 +1,39 @@
 //sol
+/*
+stragegy : https://www.educative.io/courses/grokking-coding-interview-patterns-java/N0J0qzNQ1X2
+
+Things to note: 
+1. Array will either be left sorted or right sorted
+2. We will move either in left half or right half
+3. We move in any half only if target lies in that half
+
+
+*/
 class Solution 
 { 
-    static int Search(int a[], int target)
-	{
-	    // code here
-	    int i=binarySearch(a,0,a.length-1,target);
-	    return i;
-	    
-	}
-	static int binarySearch(int a[],int start,int end,int key) {
-	    int mid=(start+end)>>1;
-	    
-	    boolean leftsorted=false,rightsorted=false;
-	    if(start>end) return -1;
-	    
-	    if(a[mid]>a[start]) leftsorted=true;
-	    if(a[mid]<a[end]) rightsorted=true; 
-	    
-	    if(a[mid]==key) return mid;
-	    if(rightsorted) {
-	        if(a[mid]<key && a[end]>=key) {
-	            return binarySearch(a,mid+1,end,key);
-	        }
-	        else {
-	            return binarySearch(a,start,mid-1,key);
-	        }
-	    }
-	    else if(leftsorted) {
-	        if(a[mid]>key && a[start]<=key) {
-	            return binarySearch(a,start,mid-1,key);
-	        }
-	        else {
-	            return binarySearch(a,mid+1,end,key);
-	        }
-	    }
-	    else {
-	        return -1;
-	    }
-	}
+    public static int binarySearchRotated(List<Integer> nums, int target) {
+      
+      // Write your code here
+      int lo = 0, hi = nums.size()-1;
+      while (lo<=hi) {
+         int mid = (lo + hi)>>1;
+         if (nums.get(mid) == target) 
+            return mid;
+         if (nums.get(lo) < nums.get(mid)) { // left sorted
+            if (nums.get(lo) <= target && nums.get(mid) > target) { // in range and mid > target
+               hi = mid-1;
+            } else {
+               lo = mid+1;
+            }
+         } else { // right sorted
+            if (nums.get(hi) >= target && nums.get(mid) < target) { // in range and mid < target
+               lo = mid+1;
+            } else {
+               hi = mid-1;
+            }
+         }
+      }
+      
+      return -1;
+   }
 } 
