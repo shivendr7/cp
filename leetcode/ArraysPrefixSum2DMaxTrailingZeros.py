866c3b027bf9aa6636125824fffdc51f29fcd40d
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2023-02-11 15:18:54 +0530

    Update ArraysPrefixSum2DMaxTrailingZeros.py

--- leetcode/ArraysPrefixSum2DMaxTrailingZeros.py
+++ leetcode/ArraysPrefixSum2DMaxTrailingZeros.py
@@ -3,7 +3,10 @@ https://leetcode.com/problems/maximum-trailing-zeros-in-a-cornered-path/
 
 You are given a 2D integer array grid of size m x n, where each cell contains a positive integer.
 
-A cornered path is defined as a set of adjacent cells with at most one turn. More specifically, the path should exclusively move either horizontally or vertically up to the turn (if there is one), without returning to a previously visited cell. After the turn, the path will then move exclusively in the alternate direction: move vertically if it moved horizontally, and vice versa, also without returning to a previously visited cell.
+A cornered path is defined as a set of adjacent cells with at most one turn. More specifically, the path should exclusively move 
+either horizontally or vertically up to the turn (if there is one), without returning to a previously visited cell. After the turn, 
+the path will then move exclusively in the alternate direction: move vertically if it moved horizontally, and vice versa, also without 
+returning to a previously visited cell.
 
 The product of a path is defined as the product of all the values in the path.
 
