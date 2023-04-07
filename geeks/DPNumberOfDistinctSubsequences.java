370d80b77c465604967510dc146cea1afc9c57c0
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2023-04-07 18:46:48 +0530

    Update DPNumberOfDistinctSubsequences.java

--- geeks/DPNumberOfDistinctSubsequences.java
+++ geeks/DPNumberOfDistinctSubsequences.java
@@ -19,7 +19,8 @@ Output: 4
 Explanation: The four distinct
 subsequences are "", "g", "gg", "ggg"
 Your task:
-You do not need to read any input or print anything. The task is to complete the function distinctSubsequences(), which takes a string as input and returns an integer.
+You do not need to read any input or print anything. The task is to complete the function distinctSubsequences(), which takes a string as input 
+and returns an integer.
 
 Expected Time Complexity: O(|str|)
 Expected Auxiliary Space: O(|str|)
