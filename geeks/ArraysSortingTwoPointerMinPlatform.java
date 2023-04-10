6fd647f57bbb8f2807eaf504c5a0b5a030d93701
Shivendra Pratap Singh <59742488+shivendr7@users.noreply.github.com>
2023-04-10 23:55:01 +0530

    Update and rename ArraysMinPlatform.java to ArraysSortingTwoPointerMinPlatform.java

--- geeks/ArraysSortingTwoPointerMinPlatform.java
+++ geeks/ArraysSortingTwoPointerMinPlatform.java
@@ -71,3 +71,70 @@ class Solution
     }
     
 }
+
+/*
+https://practice.geeksforgeeks.org/problems/63c232252d445a377e01cd91adfd7d1060580038/1
+
+N horizontal line segments are arranged on the X-axis of a 2D plane. The start and end point of each line segment is given in a Nx2 matrix lines[ ][ ]. Your task is to find the maximum number of intersections possible of any vertical line with the given N line segments.
+
+Example 1:
+
+Input:
+N = 4
+lines[][] = {{1,3}, {2,3}, {1,2}, {4,4}}
+Output:
+3
+Explanation:
+A vertical line at X = 2 passes through 
+{1,3}, {2,3}, {1,2}, ie three of the 
+given horizontal lines.
+Example 2:
+
+Input: 
+N = 3
+lines[][] = {{1, 3}, {5, 6}, {3,4}}
+Output:
+2
+Explanation: 
+A vertical line at X = 3 passes through 
+two of the given horizontal lines which 
+is the maximum possible.
+Your Task:
+You dont need to read input or print anything. Complete the function maxIntersections() which takes the 2D Matrix lines[][] and the integer N as input parameters, and returns the maximum intersections possible.
+
+Expected Time Complexity: O(N*log(N))
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ N ≤ 105 
+-109 ≤ lines[i][0] ≤ 109
+lines[i][0] ≤ lines[i][1] ≤ 109
+
+
+*/
+// sol
+/*
+
+class Solution: 
+    def maxIntersections(self, lines, N):
+        # Code here 
+        s = []
+        e = []
+        for st, en in lines:
+            s.append(st)
+            e.append(en)
+        s.sort()
+        e.sort()
+        i, j = 0, 0
+        l, ans = 0, 0
+        while i<len(s) and j<len(e):
+            if s[i]<=e[j]:
+                l += 1
+                ans = max(ans, l)
+                i += 1
+            else:
+                j += 1
+                l -= 1
+        return ans
+
+*/
